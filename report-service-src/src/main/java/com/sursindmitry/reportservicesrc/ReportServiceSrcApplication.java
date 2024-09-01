87d6cc1ee0f9bd7bdfddc942d2d35623e95e71de
Dmitry Sursin <dmitrijsursin@gmail.com>
2024-09-01 19:14:01 +0700

    PRJ-115: Инициализация проекта

---
+++ report-service-src/src/main/java/com/sursindmitry/reportservicesrc/ReportServiceSrcApplication.java
@@ -0,0 +1,13 @@
+package com.sursindmitry.reportservicesrc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportServiceSrcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportServiceSrcApplication.class, args);
+    }
+
+}
---
+++ report-service-src/src/test/java/com/sursindmitry/reportservicesrc/ReportServiceSrcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sursindmitry.reportservicesrc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportServiceSrcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
