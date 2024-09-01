14668318965e956446149b643c4cf04a68a4dda7
Dmitry Sursin <40328886+sursindmitry@users.noreply.github.com>
2024-09-01 20:13:40 +0700

    PRJ-116: Add checkstyle (#8)

---
+++ readme.md
@@ -0,0 +1,1 @@
+# Platform Report Service
--- report-service-src/src/main/java/com/sursindmitry/reportservicesrc/ReportServiceSrcApplication.java
+++ report-service-src/src/main/java/com/sursindmitry/reportservicesrc/ReportServiceSrcApplication.java
@@ -3,6 +3,9 @@ package com.sursindmitry.reportservicesrc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class.
+ */
 @SpringBootApplication
 public class ReportServiceSrcApplication {
 
