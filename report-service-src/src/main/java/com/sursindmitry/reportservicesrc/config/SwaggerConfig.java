17f041773c08bd0e627d04a145c15b317953f2a6
Dmitry Sursin <dmitrijsursin@gmail.com>
2024-09-13 23:56:23 +0700

    PRJ-118: Добавил тестовый контроллер, подключил Swagger

---
+++ report-service-src/src/main/java/com/sursindmitry/reportservicesrc/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.sursindmitry.reportservicesrc.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Swagger configuration.
+ * <p>
+ * Класс конфигурации свагера
+ * </p>
+ */
+@Configuration
+public class SwaggerConfig {
+
+    /**
+     * Настройка OpenAPI.
+     *
+     * @return {@link OpenAPI}
+     */
+    @Bean
+    public OpenAPI apiInfo() {
+
+        Info apiInfo = new Info()
+            .title("Report Service REST API")
+            .description("API for report-service. Данный сервис служит для генерации отчётов.")
+            .version("0.0.1");
+        final Server localServer = new Server().description("Локальное окружение")
+            .url("http://localhost:9292");
+
+        return new OpenAPI()
+            .info(apiInfo)
+            .servers(List.of(localServer));
+    }
+}
