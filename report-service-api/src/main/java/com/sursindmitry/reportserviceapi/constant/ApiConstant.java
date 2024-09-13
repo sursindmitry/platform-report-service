07b42cfcecff6c72484d456e996db76094ba005b
Dmitry Sursin <40328886+sursindmitry@users.noreply.github.com>
2024-09-13 23:59:09 +0700

    Develop (#12)
    * PRJ-116: Add checkstyle (#8)
    
    * PRJ-117: Add github workflows (#10)
    
    * PRJ-118: Добавил тестовый контроллер, подключил Swagger (#11)

---
+++ report-service-api/src/main/java/com/sursindmitry/reportserviceapi/constant/ApiConstant.java
@@ -0,0 +1,16 @@
+package com.sursindmitry.reportserviceapi.constant;
+
+/**
+ * ApiConstant.
+ * <p>
+ * Класс api констант
+ * </p>
+ */
+public class ApiConstant {
+
+    public static final String HELLO_API = "/api/v1/hello";
+
+    private ApiConstant() {
+
+    }
+}
